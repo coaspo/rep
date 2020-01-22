2bc025caed6deba082978ba003045b30a760a1e3
coaspo <coaspo@gmail.com>
2020-01-22 10:48:35 -0500

    correction

--- quizzer/check-in-br1.bat
+++ quizzer/check-in-br1.bat
@@ -1,11 +1,6 @@
 REM To run this in the Git Bash window type:
 REM    cmd "/C check-in-br1.bat"
 REM
-REM NOTE this runs the tests, reports
-REM "failed was unexpected at this time." and stops.
-REM pystest works from the command line
-REM but not from a bat file???
-REM
 REM For any errors, may need to update a pkg, for example:
 REM    pip install -U pluggy
 REM and recreate venv:
@@ -13,18 +8,23 @@ REM    virtualenv venv
 
 @echo on
 call .\venv\Scripts\activate.bat
+if ERRORLEVEL 1 (
+    echo --- activate venv failed
+    pause
+	exit 1
+)
 echo --- venv activated
 set PYTHONPATH=.
 call pytest tests/
 if ERRORLEVEL 1 (
     echo --- tests failed
     pause
-	set errorflag=1
+	exit 1
 )
 echo --- pytest done
 call .\venv\Scripts\deactivate.bat
 if ERRORLEVEL 1 (
-	echo. & echo    Tests have failed - cannot do check-in & echo.
+	echo --- deactivate venv failed
 	pause
 	exit 1
 )
@@ -38,9 +38,3 @@ git commit -m "%msg%"
 git push origin br1
 pause
 
-call .\venv\Scripts\activate.bat
-set PYTHONPATH=.
-pytest tests/
-if ERRORLEVEL 1 (
-	pause
-)
