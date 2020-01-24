40368ee089a978f8331ea012529bc1f4f9af7712
coaspo <coaspo@gmail.com>
2020-01-24 13:28:53 -0500

    "automate tests"

---
+++ quizzer/run_tests_and_check_into_br1.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from datetime import datetime
+from subprocess import Popen, PIPE
+
+LOG_FILE = os.path.basename(__file__) + '.log'
+
+
+def run(args: list):
+    print('cmd:', args)
+    with open(LOG_FILE, 'a') as f1:
+        f1.write('\n' + str(args))
+
+    p = Popen(args, shell=True, stdout=PIPE, stderr=PIPE)
+    o, e = p.communicate()
+    output = o.decode("utf-8").replace('\r', '')
+    errs = e.decode("utf-8").replace('\r', '')
+
+    with open(LOG_FILE, 'a') as f2:
+        if len(output) > 0:
+            f2.write('\n' + output)
+            print(output)
+        if len(errs) > 0:
+            f2.write('\n' + 15 * 'ERR---' + '\n' + errs)
+            print(15 * 'ERR---', '\n', errs)
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    with open(LOG_FILE, 'w') as f:
+        f.write(str(datetime.now()))
+    run([r'..\python-venv\Scripts\activate.bat'])
+    run(['pytest', 'tests/'])
+    run(['git', 'add', '*'])
+    run(['git', 'status'])
+    msg = input("Git commit msg: ")
+    run(['git', 'commit', '-m', '"' + msg + '"'])
+    run(['git', 'push', 'origin', 'brw'])
+    run([r'..\python-venv\Scripts\deactivate.bat'])
