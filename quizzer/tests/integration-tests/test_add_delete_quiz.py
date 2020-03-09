364c300b71fcabc36912f48d09829d86948c770b
coaspo <coaspo@gmail.com>
2020-03-08 21:03:58 -0400

    add integration test

--- quizzer/tests/integration-tests/test_add_delete_quiz.py
+++ quizzer/tests/integration-tests/test_add_delete_quiz.py
@@ -15,7 +15,7 @@ v: View
 c: QuizController
 
 
-def test_next_previous_quiz():
+def test_add_quiz():
     global m, v, c
     m = Model(TMP_DIR)
     v = View(m.latest_quiz_topic, m.quiz_topics, 'fake instructions')
@@ -29,24 +29,37 @@ def test_next_previous_quiz():
                                               '+ = 2\n'
                                               '- = 4\n\n')
     c._add_new_quiz('fake-<Button-1>-event')
+    assert v.status_label.cget('text').startswith('Saved quiz file')
+    assert v.quiz_description_label.cget('text').startswith('1/1  quiz.1.json')
+    assert 'What is 2+3' == v.question_label.cget('text')
+    assert 'addition' == v.question_comment_label.cget('text')
+
     v.input_marked_text_area.delete('1.0', tkinter.END)
     v.input_marked_text_area.insert('insert', '?What is 10+10\n'
                                               '-30\n'
                                               '+20\n\n'
                                               '=big addition\n\n')
     c._add_new_quiz('fake-<Button-1>-event')
-
-    c._next_quiz('fake-<Button-1>-event')
+    assert v.status_label.cget('text').startswith('Saved quiz file')
     assert v.quiz_description_label.cget('text').startswith('2/2  quiz.2.json')
     assert 'What is 10+10' == v.question_label.cget('text')
     assert 'big addition' == v.question_comment_label.cget('text')
 
-    c._previous_quiz('fake-<Button-1>-event')
-    assert v.quiz_description_label.cget('text').startswith('1/2  quiz.1.json')
-    assert 'What is 2+3' == v.question_label.cget('text')
-    assert 'addition' == v.question_comment_label.cget('text')
 
-    c._previous_quiz('fake-<Button-1>-event')
-    assert v.quiz_description_label.cget('text').startswith('1/2  quiz.1.json')
+def test_delete_quiz():
+    global m, v, c
+    m = Model(TMP_DIR)
+    v = View(m.latest_quiz_topic, m.quiz_topics, 'fake instructions')
+    c = QuizController(v, m)
+    assert v.status_label.cget('text').startswith('Read quiz file')
+    assert v.quiz_description_label.cget('text').startswith('2/2  quiz.2.json')
+    assert 'What is 10+10' == v.question_label.cget('text')
+    assert 'big addition' == v.question_comment_label.cget('text')
+
+    c.model.delete_quiz()
+    c._populate_quiz_widgets()
+    assert v.status_label.cget('text').startswith('Deleted quiz file')
+    assert v.status_label.cget('text').find('Read quiz file') > -1
+    assert v.quiz_description_label.cget('text').startswith('1/1  quiz.1.json')
     assert 'What is 2+3' == v.question_label.cget('text')
     assert 'addition' == v.question_comment_label.cget('text')
