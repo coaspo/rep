f5415cd57395a615519edbca1e25d82655af9acc
coaspo <coaspo@gmail.com>
2020-08-18 21:13:49 -0400

    'update'

--- w/check_in.py
+++ w/check_in.py
@@ -3,9 +3,9 @@ import pi.update_and_checkin
 import logging
 import atexit
 
-
+logging_file_name = __file__ + '.log'
 def exit_handler():
-    with open(__file__ + '.log') as f:
+    with open(logging_file_name) as f:
         return f.read()
 
 
@@ -18,10 +18,10 @@ def config_log():
     else:
         log_level = logging.DEBUG
         log_format = '%(asctime)s - [%(levelname)s] - %(name)s - %(filename)s.%(funcName)s(%(lineno)d) - %(message)s'
-    logging.basicConfig(filename=__file__ + '.log', filemode='w', level=log_level, format=log_format)
+    logging.basicConfig(filename=logging_file_name, filemode='w', level=log_level, format=log_format)
 
 
 if __name__ == '__main__':
     atexit.register(exit_handler)
     config_log()
-    pi.update_and_checkin.main('br1', __file__ + '.log')
+    pi.update_and_checkin.main('br1', logging_file_name)
