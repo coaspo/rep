454149bf7f0c000d3f127c49c282a6117942e515
coaspo <coaspo@gmail.com>
2020-08-18 21:22:42 -0400

    'update'

--- w/check_in.py
+++ w/check_in.py
@@ -1,14 +1,27 @@
+import shutil
 import sys
+from datetime import datetime
+from os import mkdir
+
 import pi.update_and_checkin
 import logging
 import atexit
 
 logging_file_name = __file__ + '.log'
+
 def exit_handler():
+    archive_log()
     with open(logging_file_name) as f:
-        return f.read()
+        print(f.read())
 
 
+def archive_log():
+    archive_dir = './logs-check-ins'
+    if not sys.path.isdir(archive_dir):
+        mkdir(archive_dir)
+    log_archive_file = archive_dir + '/' + logging_file_name + '-' + \
+                       str(datetime.now()).replace(':', '-') + '.log'
+    shutil.copyfile(logging_file_name, log_archive_file)
 
 
 def config_log():
