a7ffccb8b81edd6a8497de8f030c815db363d59e
coaspo <coaspo@gmail.com>
2025-01-02 23:00:35 -0500

    'br1'

---
+++ w/js/air-index.js
@@ -0,0 +1,43 @@
+const apiUrl = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=02180&distance=25&API_KEY=EB95D738-3455-4362-80EE-6E992757E8ED';
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Create arrays to store Parameter Names and AQI values
+        const parameterNames = [];
+        const aqiValues = [];
+        var reportingArea
+        // Iterate over the data and extract the ParameterName and AQI
+        data.forEach(item => {
+            if (item.ParameterName && item.AQI !== undefined) {
+                parameterNames.push(item.ParameterName);
+                aqiValues.push(item.AQI);
+                reportingArea = item.ReportingArea
+            }
+        });
+        let html =  reportingArea
+        for (let i = 0; i < parameterNames.length; i++) {
+           html += ' '+ parameterNames[i]+ ': '+ colorCoded(aqiValues[i])
+        }
+        document.getElementById("airIndex").innerHTML = html;
+    })
+    .catch(error => {
+        const tableBody = document.querySelector('#aqTable tbody');
+        tableBody.innerHTML = `<tr><td colspan="2">Error fetching data: ${error.message}</td></tr>`;
+    });
+
+function colorCoded(airIndex) {
+    let backColor = '00e400'
+    index = parseInt(airIndex);
+    if (index > 50 && index <100) {
+       backColor = 'ffff00'
+    } else if (index >99) {
+       backColor = 'ff0000'
+    }
+    html = '<span style="background-color:#'+ backColor + '">' + airIndex + '</span>'
+    return html
+}
